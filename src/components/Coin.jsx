7ea08647d14ea88af1e33e05fb33c3e08d925bd6
emirjaqem <emirjaqem@gmail.com>
2023-04-05 23:44:24 +0300

    coin header added ath added

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-function Coin({name, image, symbol, price, volume, priceChange, marketcap}) {
+function Coin({id,name, image, symbol, price, volume, priceChange, marketcap, rank ,ath}) {
   return (
     <div className='coin-container'>
      <div className="coin-row">
-        <div className="coin">
+        <div className="coin" key={id}>
+          <p className='coin-rank'>{rank}</p>
         <img src={image} alt="crypto" />
-        <h1>{name}</h1>
-        <p className='coin-symbol'>{symbol}</p>
+        <h1 className='h1'>{name} <span className='coin-symbol'>({symbol})</span> </h1>
         </div>
         <div className="coin-data">
             <p className="coin-price">${price}</p>
-            <p className='coin-volume'>${volume.toLocaleString()}</p>
             {priceChange < 0 ? (
                 <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
             ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>) }
+            <p className='coin-volume'>${volume.toLocaleString()}</p>
+            <p className='coin-ath'>${ath}</p>
             <p className="coin-marketcap">
-                Market Cap: ${marketcap.toLocaleString()}
+                ${marketcap.toLocaleString()}
             </p>
         </div>
      </div>
